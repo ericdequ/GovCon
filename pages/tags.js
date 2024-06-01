caf84a11cae7685a3656b21dc6415ae071a68237
Eric <ericdequevedo@gmail.com>
2024-06-01 01:17:36 -0400

    lint

--- pages/tags.js
+++ pages/tags.js
@@ -6,7 +6,6 @@ import { getAllTags } from '@/lib/tags'
 import kebabCase from '@/lib/utils/kebabCase'
 import { motion } from 'framer-motion'
 
-
 export async function getStaticProps() {
   let tags = {}
   try {
@@ -44,13 +43,12 @@ export default function Tags({ tags }) {
 
   return (
     <>
-      <PageSEO title={`Projects - ${siteMetadata.author}`} description={siteMetadata.description} />
+      <PageSEO title={`Tags - ${siteMetadata.author}`} description="Things I blog about" />
       <motion.div
-        className="flex overflow-x-hidden overflow-y-hidden flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0"
+        className="flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0"
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5 }}
-       
       >
         <div className="space-x-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:border-r-2 md:px-6 md:text-6xl md:leading-14">
@@ -58,7 +56,7 @@ export default function Tags({ tags }) {
           </h1>
         </div>
         <motion.div
-          className="flex max-w-lg flex-wrap"
+          className="flex flex-wrap justify-center"
           variants={containerVariants}
           initial="hidden"
           animate="visible"
@@ -67,14 +65,14 @@ export default function Tags({ tags }) {
           {sortedTags.map((t) => {
             const tagSlug = kebabCase(t)
             return (
-              <motion.div key={tagSlug} className="mt-2 mb-2 mr-5" variants={tagVariants}>
+              <motion.div key={tagSlug} className="m-2" variants={tagVariants}>
                 <Tag text={t} />
-                <div
+                <Link
                   href={`/tags/${tagSlug}`}
-                  className="ml-3 P-3 text-sm font-semibold uppercase text-gray-600 dark:text-gray-300"
+                  className="ml-2 text-sm font-semibold uppercase text-gray-600 dark:text-gray-300"
                 >
                   {`(${tags[t]})`}
-                </div>
+                </Link>
               </motion.div>
             )
           })}
