33e7705828d0dc2f0e5a5d74fe490d593dc2080f
Eric deQuevedo <ericdequevedo@gmail.com>
2025-12-30 11:16:10 -0800

    vercel logo and proj liks

--- data/projectsData.js
+++ data/projectsData.js
@@ -8,26 +8,20 @@ const projectsData = [
     url: 'https://quantumcybersolutions.com',
     imgSrc: 'https://www.quantumcybersolutions.com/QCS-logo.webp',
   },
-  {
-    title: 'Devote',
-    description:
-      'A web application backed by blockchain that helps users make direct votes for issues in their community.',
-    url: 'https://www.robotric.org/Classroom/',
-    imgSrc: 'https://www.quantumcybersolutions.com/entanglement.webp',
-  },
+
   {
     title: 'develope',
     description:
       'A web based application for communities to vote on what gets built in their community, simulations to see how the building affects, property value, lifestyle.',
     url: 'https://www.robotric.org/Tech/ATL/democracy-app',
-    imgSrc: 'https://www.quantumcybersolutions.com/develop.webp',
+    imgSrc: 'https://www.robotric.org/Realms/Gods/renders_realm/government-parties-plaza.png',
   },
   {
     title: 'Royalwee',
     description:
       'A Democratic monarchy that allows communities, cities to elect local leaders as Culture, Art, Tech, Queens, & kings for the more nueanced opportunities, Also helps bring attention to local politics',
-    url: 'https://www.govcon.me/articleimage/Relations/BuildingABetterFuture.webp',
-    imgSrc: 'https://rikara.vercel.app/innovate/5.png',
+    url: 'https://www.robotric.org/Realms/Atlantis',
+    imgSrc: 'https://www.robotric.org/innovate/5.png',
   },
 
 ]
